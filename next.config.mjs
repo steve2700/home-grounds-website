484b8a3655ece0a2ce7a7023cec5b9819846b357
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2026-02-12 05:10:22 -0800

    Update next.config.mjs for static export settings
    Enable static export and trailing slash in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable static export (VERY IMPORTANT)
+  output: "export",
+  trailingSlash: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
- 
-}
+};
 
-export default nextConfig
+export default nextConfig;
